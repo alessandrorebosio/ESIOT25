eee0ad1ab2f5c0b31a675a9dec9bd967e2b572ed
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2025-10-12 17:57:13 +0200

    fix: changed file from .c to .cpp and min function

--- assignment-01/src/tos/tos.ino
+++ assignment-01/src/tos/tos.ino
@@ -4,7 +4,7 @@
 const int BUTTON[] = {5, 4, 3, 2};
 const int LED[] = {9, 8, 7, 6};
 
-int len = min(LEN(BUTTON), LEN(LED));
+int len = MIN(LEN(BUTTON), LEN(LED));
 
 void setup() {
     // put your setup code here, to run once:
