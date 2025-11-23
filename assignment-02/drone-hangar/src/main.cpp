e95b3a741588ea0d25c80d8fb6bdc76602e65ca3
Grazia Bochdanovits <grazia.bochdanovits@studio.unibo.it>
2025-11-23 22:12:32 +0100

    update setUp

--- assignment-02/drone-hangar/src/main.cpp
+++ assignment-02/drone-hangar/src/main.cpp
@@ -1,20 +1,42 @@
 #include <Arduino.h>
 
 #include "core/Scheduler.h"
+#include "core/MsgService.h"
 
 #include "model/HWPlatform.h"
+#include "model/Context.h"
 
 #include "tasks/BlinkingTask.h"
+#include "tasks/HangarControlTask.h"
+#include "tasks/DistanceMeasuringTask.h"
+#include "tasks/SweepingTask.h"
 
 #include "config.h"
 
 Scheduler scheduler;
 HWPlatform *hw;
+Context *context;
+MsgSerivce *msg;
 
 void setup() {
     hw = new HWPlatform();
+    context = new Context();
+    msg = new MsgSerivce(BAUD);
+
+    Task *hangarControl = new HangarControlTask(hw, msg, context);
+    hangarControl->init(DEFAULT_PERIOD);
+
+    Task *sweepingDoor = new SweepingTask(hw->getMotor(), context);
+    sweepingDoor->init(DEFAULT_PERIOD);
+
+    Task *measuringDist = new DistanceMeasuringTask(hw->getSonar(), msg, context);
+    measuringDist->init(DEFAULT_PERIOD);
 
     scheduler.init(DEFAULT_PERIOD);
+
+    scheduler.addTask(hangarControl);
+    scheduler.addTask(sweepingDoor);
+    scheduler.addTask(measuringDist);
 }
 
 void loop() {
