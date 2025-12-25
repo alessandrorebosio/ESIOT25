5e5b0d6a9bc4977f4856e633b6394af0b8c408ad
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2025-12-25 02:35:43 +0100

    aded wifi setup esp32

--- assignment-03/tms/src/main.cpp
+++ assignment-03/tms/src/main.cpp
@@ -1,6 +1,30 @@
 #include <Arduino.h>
 
+#include "WiFi.h"
+#include "config.h"
+
+const char *ssid = "";
+const char *password = "";
+
+void setup_wifi() {
+    Serial.println(String("Connecting to ") + ssid);
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
 void setup() {
+    setup_wifi();
 }
 
 void loop() {
