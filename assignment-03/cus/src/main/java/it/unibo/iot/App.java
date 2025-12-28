4f72df74ca9a753e2eea95d715df2d50a3014e4e
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2025-12-28 23:44:02 +0100

    added gradlew cus

---
+++ assignment-03/cus/src/main/java/it/unibo/iot/App.java
@@ -0,0 +1,22 @@
+package it.unibo.iot;
+
+/**
+ * Entry point of the app.
+ * 
+ * @author Alessandro Rebosio
+ * @since 1.0
+ */
+public final class App {
+
+    private App() {
+    }
+
+    /**
+     * Starts the application.
+     *
+     * @param args not used
+     */
+    public static void main(final String[] args) {
+        System.out.println("ciao");
+    }
+}
