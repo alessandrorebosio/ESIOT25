632bce5446c9c1345573e85621bd889470dce46e
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2025-12-30 01:57:40 +0100

    minor changes

--- assignment-03/water-channel-subsystem/include/core/Context.h
+++ assignment-03/water-channel-subsystem/include/core/Context.h
@@ -1,14 +1,34 @@
 #pragma once
 
+namespace System {
+enum Print { NONE, AUTOMATIC, MANUAL };
+}
+
 class Context final {
   private:
     bool automatic;
+    bool print = true;
+
+    System::Print systemPrint : 2;
+
+    void setToPrint(System::Print value);
 
   public:
     /**
      * @brief Constructs a new Context object.
      */
     Context(void);
+
+    void printAutomatic(void);
+    void printManual(void);
+
+    void clearPrint(void);
+
+    /**
+     * @brief Get print request status.
+     * @return Constant reference to print flag.
+     */
+    const bool &shouldPrint(void);
 
     void setAutomatic(void);
     void setManual(void);
--- assignment-03/water-channel-subsystem/src/core/Context.cpp
+++ assignment-03/water-channel-subsystem/src/core/Context.cpp
@@ -11,6 +11,32 @@ void Context::reset(void) {
     this->setAutomatic();
 }
 
+void Context::setToPrint(System::Print value) {
+    this->systemPrint = value;
+    this->print = true;
+}
+
+void Context::printAutomatic(void) {
+    this->setToPrint(System::Print::AUTOMATIC);
+}
+
+void Context::printManual(void) {
+    this->setToPrint(System::Print::MANUAL);
+}
+
+void Context::clearPrint(void) {
+    this->systemPrint = System::Print::NONE;
+    this->print = false;
+}
+
+/**
+ * @brief Get print request status.
+ * @return Constant reference to print flag.
+ */
+const bool &Context::shouldPrint(void) {
+    return this->print;
+}
+
 void Context::setAutomatic(void) {
     this->automatic = true;
 }
